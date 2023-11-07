340e60e600daf12018104a0492698d6be288adcf
Velicu Marius <velicumarius21@stud.ase.ro>
2023-11-08 01:16:27 +0200

    add homework out of zip file

---
+++ app.js
@@ -0,0 +1,44 @@
+//Velicu Marius - 1097E
+const InvalidType = new Error('InvalidType');
+
+const rle = (text, compress = true) => {
+  //TODO: implementați funcția
+  // TODO: implement the function
+
+  if(typeof text !== 'string' && !(text instanceof String))
+    throw InvalidType;
+
+  if (compress === true)
+    return RLECompression(text);
+  else
+    return RLEDecompression(text);
+
+}
+
+function RLECompression(text) {
+  compressText = '';
+  ct = 1;
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === text[i + 1]) {
+      ct++;
+    }
+    else {
+      compressText += text[i] + ct;
+      ct = 1;
+    }
+  }
+  return compressText;
+}
+
+function RLEDecompression(text) {
+  decompressText = '';
+  for (var i = 1; i < text.length; i = i + 2) {
+    letter = text[i - 1];
+    for (var j = 0; j < parseInt(text[i]); j++)
+      decompressText += letter;
+  }
+  return decompressText;
+}
+
+module.exports = rle
+
